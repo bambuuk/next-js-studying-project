bc4f331a38020579d8609617470077b73f7aec8d
Ivan <vanyok.korobka@ukr.net>
2023-12-25 00:21:42 +0200

    prepare fetching for Blog page and for dynamic page

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -7,16 +7,30 @@ type Props = {
 }
 
 export async function generateMetadata({ params: { id } }: Props): Promise<Metadata> {
+  const post = await getData(id);
+
   return {
-    title: id,
+    title: post.title,
   }
 }
 
-const Post = ({ params: { id } }: Props) => {
+async function getData(id: string) {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    next: {
+      revalidate: 60,
+    }
+  });
+  return response.json();
+}
+
+const Post = async ({ params: { id } }: Props) => {
+  const post = await getData(id);
+
   return (
-    <h2>
-      Post page {id}
-    </h2>
+    <>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </>
   )
 }
 
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import { Metadata } from "next";
+import Link from "next/link";
 
-const BlogPage = () => {
+async function getData() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+    next: {
+      revalidate: 60,
+    }
+  });
+  return response.json();
+}
+
+export const metadata: Metadata = {
+  title: 'Blog',
+}
+
+const BlogPage = async () => {
+  const posts = await getData();
   return (
-    <h1>
-      Blog Page
-    </h1>
+    <>
+      <h1>Blog Page</h1>
+      <ul>
+        {posts.map((post: any) => (
+          <li key={post.id}>
+            <Link href={`/blog/${post.id}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
   )
 }
 
