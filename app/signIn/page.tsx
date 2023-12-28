a6acdc0b3a763be992633fd9e767b0a1c1059975
Ivan <vanyok.korobka@ukr.net>
2023-12-28 21:58:14 +0200

    prepare authentication with email password (credentials) and create users array with info about users for auth

---
+++ app/signIn/page.tsx
@@ -0,0 +1,11 @@
+
+
+const SignIn = async () => {
+  return (
+    <div>
+      <h1>SignIn</h1>
+    </div>
+  )
+}
+
+export default SignIn
---
+++ data/users.ts
@@ -0,0 +1,23 @@
+export const users = [
+  {
+    id: "1",
+    email: "ikorobka2077@gmail.com",
+    name: "Bambuuk",
+    password: "12345",
+    role: "admin",
+  },
+  {
+    id: "2",
+    email: "vanyok.korobka@ukr.net",
+    name: "ivan",
+    password: "12345",
+    role: "admin",
+  },
+  {
+    id: "3",
+    email: "any@gmail.com",
+    name: "Just a Guest",
+    password: "12345",
+    role: "guest",
+  },
+];
